6ff074717cbca2ef19c9dd739d58dedf4034fe17
minamiaomi <minamiaomi@LAPTOP-1K3GK5JA>
2023-10-23 12:55:45 +0900

    修正

--- main.cpp
+++ main.cpp
@@ -13,42 +13,36 @@ int RollDice() {
     return std::uniform_int_distribution<int>(1, 6)(g_randomGenerator);
 }
 
-typedef int (*CallBack)();
+void DispResult() {
+    int answer = RollDice();
+    std::cout << "答えは : " << answer << std::endl;
+    if (answer % 2 == 0) {
+        std::cout << "偶数です。" << std::endl;
+        return;
+    }
+    std::cout << "奇数です。" << std::endl;
+}
+
+typedef void (*CallbackFunc)();
+
+void SetTimeout(CallbackFunc func, size_t second) {
+    std::this_thread::sleep_for(std::chrono::seconds(second));
+    func();
+}
 
 int main() {
 
-    while (true) {
-        std::cout << "終了 : 0, 奇数 : 1, 偶数 : 2 を入力してください。" << std::endl;
-        int input = 0;
-        std::cin >> input;
-        // 0の場合は終了
-        if (input == 0) {
-            std::cout << "終了します。" << std::endl;
-            break;
-        }
-        // 0,1,2以外の場合はもう一度聞く
-        if (input < 0 || input > 2) {
-            continue;
-        }
-        // コールバック関数をセット
-        CallBack callBack = &RollDice;
-
-        // 3秒待つ
-        const int kWaitTime = 3;
-        for (int i = 0; i < kWaitTime; ++i) {
-            std::cout << ". ";
-            std::this_thread::sleep_for(std::chrono::seconds(1));
-        }
-
-        int answer = callBack();
-        std::cout << "答えは : " << answer << std::endl;
-        if (answer % 2 == 0) {
-            std::cout << "偶数です。" << std::endl;
-        }
-        else {
-            std::cout << "奇数です。" << std::endl;
-        }
-
+    std::cout << "終了 : 0, 奇数 : 1, 偶数 : 2 を入力してください。" << std::endl;
+    int input = 0;
+    std::cin >> input;
+    // 0の場合は終了
+    if (input == 0) {
+        std::cout << "終了します。" << std::endl;
+    }
+    else {
+        CallbackFunc func = &DispResult;
+        // 3秒後に実行
+        SetTimeout(func, 3);
 
         std::cout << std::endl;
     }
