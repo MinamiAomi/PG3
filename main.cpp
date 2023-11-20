cb8c600278687661f43989ff3d9c215a45552c27
minamiaomi <minamiaomi@LAPTOP-1K3GK5JA>
2023-11-20 11:11:35 +0900

    修正しました。

--- main.cpp
+++ main.cpp
@@ -4,6 +4,11 @@
 #include <thread>
 #include <functional>
 
+void SetTimeout(std::function<void()> func, std::chrono::seconds seconds) {
+    std::this_thread::sleep_for(seconds);
+    func();
+}
+
 int main() {
 
     // シード値生成器
@@ -11,14 +16,29 @@ int main() {
     // 乱数生成器
     std::mt19937 randomGenerator(seedGenerator());
 
-    // ダイスを振る関数
-    std::function RollDice = [&]() {
-        return std::uniform_int_distribution<int>(1, 6)(randomGenerator);
+    int input = 0;
+    // ダイスの結果を表示
+    std::function PrintDice = [&]() {
+        int dice = std::uniform_int_distribution<int>(1, 6)(randomGenerator);
+        std::cout << "答えは : " << dice << std::endl;
+
+        if (dice % 2 == 0) {
+            std::cout << "偶数です。" << std::endl;
+        }
+        else {
+            std::cout << "奇数です。" << std::endl;
+        }
+
+        if ((dice % 2) == (input % 2)) {
+            std::cout << "正解です。" << std::endl;
+        }
+        else {
+            std::cout << "不正解です。" << std::endl;
+        }
     };
 
     while (true) {
         std::cout << "終了 : 0, 奇数 : 1, 偶数 : 2 を入力してください。" << std::endl;
-        int input = 0;
         std::cin >> input;
         // 0の場合は終了
         if (input == 0) {
@@ -30,22 +50,7 @@ int main() {
             continue;
         }
 
-        // 3秒待つ
-        const int kWaitTime = 3;
-        for (int i = 0; i < kWaitTime; ++i) {
-            std::cout << ". ";
-            std::this_thread::sleep_for(std::chrono::seconds(1));
-        }
-
-        // コールバック関数を呼び出す
-        int answer = RollDice();
-        std::cout << "答えは : " << answer << std::endl;
-        if (answer % 2 == 0) {
-            std::cout << "偶数です。" << std::endl;
-        }
-        else {
-            std::cout << "奇数です。" << std::endl;
-        }
+        SetTimeout(PrintDice, std::chrono::seconds(3));
 
         std::cout << std::endl;
     }
