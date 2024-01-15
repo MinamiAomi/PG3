21319b779a4c3230f7f7418f93051914fd1798ee
MinamiAomi <115054812+MinamiAomi@users.noreply.github.com>
2024-01-15 10:54:58 +0900

    06_02

--- main.cpp
+++ main.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
+#include <thread>
 
+void Print(int num) {
+    std::printf("thread %d\n", num);
+}
 
 int main() {
 
+    std::thread thread1(Print, 1);
+    thread1.join();
 
-    std::cout << std::endl << "このプログラムは終了しました。" << std::endl;
-
+    std::thread thread2(Print, 2);
+    thread2.join();
+    
+    std::thread thread3(Print, 3);
+    thread3.join();
+    
     (void)std::getchar();
     return 0;
 }
